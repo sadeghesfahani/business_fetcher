2607bcbb2460b989160cbbf6d8e8c58061eeac58
Sina <sadeghesfahani.sina@gmail.com>
2022-01-03 09:08:09 +0330

    dockerized

--- fetcher/models.py
+++ fetcher/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
+
 # Create your models here.
+class Business(models.Model):
+    name = models.CharField(max_length=150)
+    registry_code = models.IntegerField()
+    legal_form = models.CharField(max_length=150)
+    status = models.CharField(max_length=150)
+    registered_date = models.DateField()
+    financial_year = models.CharField(max_length=150)
+    other = models.JSONField()
+
+
+class Person(models.Model):
+    business = models.ForeignKey(Business, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    role = models.CharField(max_length=250)
+
+
+class Activity(models.Model):
+    business = models.ForeignKey(Business, on_delete=models.CASCADE)
+    type = models.CharField(max_length=150)
+    area = models.CharField(max_length=250)
+    EMTAK_code = models.CharField(max_length=250)
+    NACE_code = models.CharField(max_length=250)
+    source = models.CharField(max_length=250)
+
+
