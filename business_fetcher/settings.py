a791338453f00ef27530963ab00159c72ea7a973
Sina <sadeghesfahani.sina@gmail.com>
2022-01-03 09:26:17 +0330

    fetcher added to settings

--- business_fetcher/settings.py
+++ business_fetcher/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fetcher.apps.FetcherConfig',
 ]
 
 MIDDLEWARE = [
@@ -70,17 +69,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'business_fetcher.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -100,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -112,7 +108,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
@@ -122,3 +117,27 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# DATABASES = {"default": "postgres://sadeghesfahani.sina@gmail.com:sadeghesfahani.sina@gmail.com@business_fetcher_database:5432/business_fetcher"}
+# DATABASES["default"]["ATOMIC_REQUESTS"] = True
+
+
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': "business_fetcher",
+
+        'USER': 'admin',
+
+        'PASSWORD': 'admin',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
+    }
+
+}
--- fetcher/admin.py
+++ fetcher/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Business, Person, Activity
 # Register your models here.
+
+
+admin.site.register(Business)
+admin.site.register(Person)
+admin.site.register(Activity)
