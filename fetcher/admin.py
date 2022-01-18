e698fc8501755b60eb0bc5b53fb170e5a3e56fdd
Sina <sadeghesfahani.sina@gmail.com>
2022-01-18 07:28:48 +0330

    admin page designed

--- fetcher/admin.py
+++ fetcher/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Business, Person, Activity, Page, Proxy, URL
 
-
 admin.site.register(Person)
 admin.site.register(Activity)
 admin.site.register(Page)
@@ -9,11 +8,17 @@ admin.site.register(Proxy)
 admin.site.register(URL)
 
 
-class BusinessAdmin(admin.ModelAdmin):
-    list_filter = ('complete',"in_process")
-    list_display = ('name',"complete",'url')
-    search_fields = ['name', 'url']
+class PersonInline(admin.TabularInline):
+    model = Person
 
+class ActivityInline(admin.TabularInline):
+    model = Activity
+
+class BusinessAdmin(admin.ModelAdmin):
+    list_filter = ('complete', "in_process", 'status')
+    list_display = ('name', "complete", 'url')
+    search_fields = ['name', 'url']
+    inlines = [PersonInline,ActivityInline ]
 
 
 admin.site.register(Business, BusinessAdmin)
